d3615bb42c6c3d21efcd7727d2521a8eaa734450
Divine C <Divine_C@ad.infosys.com>
2017-07-12 18:34:27 +0530

    committing basic rest service1

---
+++ boot-rest/src/main/java/com/rw/MainClass.java
@@ -0,0 +1,20 @@
+package com.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MainClass extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainClass.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainClass.class, args);
+
+	}
+
+}
